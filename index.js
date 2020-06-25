34368c4c76e5ec48a4ff4d528d4ac96f694cd4ef
Victor <vctrd3@gmail.com>
2020-06-25 11:16:45 -0400

    changed dimensions of maze

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 6;
-const cellsVertical = 4;
+const cellsHorizontal = 16;
+const cellsVertical = 14;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
